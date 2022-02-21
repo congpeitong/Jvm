8738f504c46eb9b88e1bd8ea7fd6bceea2733bb2
congpeitong <peitong_cong@163.com>
2022-02-21 09:53:27 +0800

    JVM的架构模型

---
+++ chapter01/src/com/study/java/StackStruTest.java
@@ -0,0 +1,20 @@
+package com.study.java;
+
+/**
+ * @createUser： congpeitong
+ * @createTime： 2022/2/21
+ * @describe： java 栈的测试
+ */
+public class StackStruTest {
+    /**
+     * 1. 进入字节码文件目录
+     * 2.反编译：指令javap -v "字节码文件名.class"
+     * @param args
+     */
+    public static void main(String[] args) {
+        int i = 2;
+        int j = 3;
+        int k = 2+3;
+
+    }
+}
