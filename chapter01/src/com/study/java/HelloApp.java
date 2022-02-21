16c3d3c12045f08408acdfbde8c845bb8fc17b5e
congpeitong <peitong_cong@163.com>
2022-02-21 14:50:17 +0800

    类加载过程

---
+++ chapter01/src/com/study/java/HelloApp.java
@@ -0,0 +1,14 @@
+package com.study.java;
+
+/**
+ * @createUser： congpeitong
+ * @createTime： 2022/2/21
+ * @describe：
+ */
+public class HelloApp {
+    private  static  int a = 1; // prepare a = 0 ---> initial: a = 1
+
+    public static void main(String[] args) {
+        System.out.println(a);
+    }
+}
---
+++ chapter01/src/com/study/java/HelloLoader.java
@@ -0,0 +1,16 @@
+package com.study.java;
+
+/**
+ * @createUser： congpeitong
+ * @createTime： 2022/2/21
+ * @describe：
+ */
+public class HelloLoader  {
+
+
+    public static void main(String[] args) {
+        System.out.println("classLoader加载了HelloLoader");
+        System.out.println("calssLoader真的加载了");
+    }
+
+}
